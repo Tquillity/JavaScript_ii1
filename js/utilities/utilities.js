49e67f669843a2f37ed435adf343437f1f95d9e6
Mikael Sundh <mikael.sundh@medieinstitutet.se>
2024-01-27 15:04:02 +0100

    utilities: added boolean data handler for JSON converter for popular course admin input

--- js/utilities/utilities.js
+++ js/utilities/utilities.js
@@ -1,5 +1,14 @@
+// =================================
+// utilities.js - provides data conversion to work with JSON-server
+// ================================= 
+
 const convertFormDataToJson = (formData) => {
     const data = Object.fromEntries(formData.entries());
+    
+    if (data.hasOwnProperty('popular')) {
+        data.popular = data.popular === 'true';
+    }
+
     return data;
 };
 
